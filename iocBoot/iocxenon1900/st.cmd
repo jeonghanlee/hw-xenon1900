661930c8d2877974e9293e941cfa6df93f8e3cb2
Jeong Han Lee <jeonghan.lee@gmail.com>
2017-05-17 15:50:44 +0200

    add the first and naive IOC

--- iocBoot/iocxenon1900/st.cmd
+++ iocBoot/iocxenon1900/st.cmd
@@ -5,8 +5,10 @@
 
 < envPaths
 
+epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST" "10.0.7.177")
+
 epicsEnvSet("STREAM_PROTOCOL_PATH", ".:${TOP}/db")
-epicsEnvSet("TTY","$(TTY=/dev/ttyAMA0)")
+epicsEnvSet("TTY","$(TTY=/dev/ttyACM0)")
 
 cd "${TOP}"
 
@@ -14,18 +16,22 @@ cd "${TOP}"
 dbLoadDatabase "dbd/xenon1900.dbd"
 xenon1900_registerRecordDeviceDriver pdbbase
 
+drvAsynSerialPortConfigure("L0", "$(TTY)", 0,0,0)
 
-drvAsynSerialPortConfigure("XENON", "$(TTY)", 0,0,0)
+# CR, "\r" , 0x0d.
+# LF, "\n" , 0x0a
 
-asynSetOption("XENON", -1, "baud", "9600")
-asynSetOption("XENON", -1, "bits", "8")
-asynSetOption("XENON", -1, "parity", "none")
-asynSetOption("XENON", -1, "stop", "1")
-asynSetOption("XENON", -1, "clocal", "Y")
-asynSetOption("XENON", -1, "crtscts", "N")
+asynOctetSetInputEos("L0", 0, "\r\n")
+#asynOctetSetOutputEos  "L0", 0, "\r"
+asynSetOption("L0", -1, "baud", "115200")
+asynSetOption("L0", -1, "bits", "8")
+asynSetOption("L0", -1, "parity", "none")
+asynSetOption("L0", -1, "stop", "1")
+asynSetOption("L0", -1, "clocal", "Y")
+asynSetOption("L0", -1, "crtscts", "N")
 
 dbLoadRecords("db/iocAdminSoft.db", "IOC=ICSLAB:IocStat")
-dbLoadRecords("db/xenon1900.db",    "SYSDEV=ICSLAB:,HWUNIT=XENON")
+dbLoadRecords("db/xenon1900.db",    "SYSDEV=ICSLAB:,PORT=L0")
 
 
 cd "${TOP}/iocBoot/${IOC}"
@@ -33,3 +39,5 @@ iocInit
 
 ## Start any sequence programs
 #seq sncxxx,"user=jhlee"
+
+dbl > "${TOP}/${IOC}_PVs.list"
